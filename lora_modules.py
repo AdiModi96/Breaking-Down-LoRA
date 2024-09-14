6375289e8a013aff71adb8b637186f07e3d7dfb6
Aditya Modi <aditya.modi.in@gmail.com>
2024-09-15 01:22:08 +0530

    Implemented nn.ParameterDict in lora_modules.py

--- lora_modules.py
+++ lora_modules.py
@@ -1,12 +1,14 @@
 import torch
 from torch import nn, Tensor
+from collections import OrderedDict
 
 
 class LoRALinear(nn.Module):
     def __init__(self) -> None:
         super(LoRALinear, self).__init__()
-        self.adapter_enabled = False # Controls the inferencing, "base" or "base + adapter"
-        self.parameters_dict = nn.ParameterDict()
+        self.base_parameters = nn.ParameterDict()
+        self.adapter_parameters = nn.ParameterDict()
+        self.adapter_enabled = False  # Controls the inferencing, "base" or "base + adapter"
 
     def enable_adapter(self) -> None:
         self.adapter_enabled = True
@@ -15,98 +17,131 @@ class LoRALinear(nn.Module):
         self.adapter_enabled = False
 
     def add_base_module(self, base_module: nn.Linear) -> None:
-        self.parameters_dict['base'] = nn.ParameterDict()
+        assert type(base_module) == nn.Linear, 'Please provide an instance on torch.nn.Linear.'
+
         for name, parameter in base_module.named_parameters():
-            parameter.requires_grad = False
-            self.parameters_dict['base'][name] = parameter
+            self.base_parameters[name] = parameter
+            self.base_parameters[name].requires_grad = False
 
     def build_new_adapter(self, lora_config: dict) -> None:
-        self.out_features, self.in_features = self.base_module.weight.size()
+        assert len(self.base_parameters) > 0, 'No base parameters found! Please add a base module of type torch.nn.Linear first.'
+        out_features, in_features = self.base_parameters['weight'].size()
 
         # Initializing tensors
-        self.alpha = torch.tensor(lora_config['alpha'])
-        self.rank = torch.tensor(lora_config['rank'], dtype=torch.int32)
-        self.delta_weight_B = nn.init.normal_(torch.empty(size=(self.out_features, lora_config['rank'])))
-        self.delta_weight_A = nn.init.normal_(torch.empty(size=(lora_config['rank'], self.in_features)))
-        self.delta_bias = None
-        if lora_config['bias'] is not None:
-            self.delta_bias = nn.init.normal_(size=(self.out_features))
+        alpha = torch.tensor(lora_config['alpha'])
+        rank = torch.tensor(lora_config['rank'], dtype=torch.int32)
+        delta_weight_B = nn.init.normal_(torch.empty(size=(out_features, lora_config['rank'])))
+        delta_weight_A = nn.init.normal_(torch.empty(size=(lora_config['rank'], in_features)))
+        delta_bias = None
+        if lora_config['delta_bias']:
+            delta_bias = nn.init.normal_(torch.empty(size=(out_features,)))
 
-        # Registering them as parameters
-        self.alpha = nn.Parameter(self.alpha, requires_grad=False)
-        self.rank = nn.Parameter(self.rank, requires_grad=False)
-        self.delta_weight_B = nn.Parameter(self.delta_weight_B, requires_grad=True)
-        self.delta_weight_A = nn.Parameter(self.delta_weight_A, requires_grad=True)
-        if lora_config['bias'] is not None:
-            self.delta_bias = nn.Parameter(self.delta_bias, requires_grad=True)
+        # Registering the tensors as parameters
+        self.adapter_parameters['alpha'] = nn.Parameter(alpha, requires_grad=False)
+        self.adapter_parameters['rank'] = nn.Parameter(rank, requires_grad=False)
+        self.adapter_parameters['delta_weight_B'] = nn.Parameter(delta_weight_B, requires_grad=True)
+        self.adapter_parameters['delta_weight_A'] = nn.Parameter(delta_weight_A, requires_grad=True)
+        if lora_config['delta_bias']:
+            self.adapter_parameters['delta_bias'] = nn.Parameter(delta_bias, requires_grad=True)
 
     def load_state_dict(self, state_dict) -> None:
-        self.out_features, self.in_features = state_dict['base_module']['weight'].size()
-        self.base_module = nn.Linear
+        for name, value in state_dict['base'].items():
+            self.base_parameters[name] = nn.Parameter(value, requires_grad=False)
 
+        for name, value in state_dict['adapter'].items():
+            if name in ['rank', 'alpha']:
+                self.adapter_parameters[name] = nn.Parameter(value, requires_grad=False)
+            else:
+                self.adapter_parameters[name] = nn.Parameter(value, requires_grad=True)
 
+    def state_dict(self, *args, destination=None, prefix='', keep_vars=False):
+        state_dict = super().state_dict(*args, destination=destination, prefix=prefix, keep_vars=keep_vars)
 
-        out_features, in_features = state_dict.get('base_module.weight').size()
-        base_module = nn.Linear(in_features=in_features, out_features=out_features)
-        base_module.weight = nn.Parameter(state_dict.get('base_module.weight'), requires_grad=False)
-        base_module.bias = nn.Parameter(state_dict.get('base_module.bias'), requires_grad=False)
-        self.add_base_module(base_module=base_module)
+        state_dict = OrderedDict()
+        state_dict['base'] = OrderedDict()
+        for name, parameter in self.base_parameters.items():
+            state_dict['base'][name] = parameter.data
 
-        self.delta_weight_B = nn.Parameter(state_dict.get('delta_weight_B'))
-        self.delta_weight_A = nn.Parameter(state_dict.get('delta_weight_A'))
-
-        self.rank = nn.Parameter(state_dict.get('rank'), requires_grad=False)
-        self.alpha = nn.Parameter(state_dict.get('alpha'), requires_grad=False)
-
-    def state_dict(self):
-        state_dict = {
-            'module_class': self.__class__,
-            'state_dict': {
-                'base_module': {
-                    'weight': self.base_module.weight
-                },
-                'adapter': {
-                    'alpha': self.alpha,
-                    'rank': self.rank,
-                    'delta_weight_B': self.delta_weight_B,
-                    'delta_weight_A': self.delta_weight_A
-                }
-            },
-        }
-
-        if 'bias' in self.base_module.state_dict().keys():
-            state_dict['state_dict']['base_module']['bias'] = self.base_module.bias
-        if self.delta_bias is not None:
-            state_dict['state_dict']['adapter']['delta_bias'] = self.delta_bias
+        state_dict['adapter'] = OrderedDict()
+        for name, parameter in self.base_parameters.items():
+                state_dict['adapter'][name] = parameter.data
 
         return state_dict
 
-    def get_base_module(self) -> nn.Linear:
-        return self.base_module
-
     def get_merged_module(self) -> nn.Linear:
-        merged_module = nn.Linear(in_features=self.in_features, out_features=self.out_features)
-        merged_module.weight = nn.Parameter(
-            self.base_module.weight + \
-            (self.alpha / self.rank) * torch.matmul(self.delta_weight_A.T, self.delta_weight_B.T))
-        merged_module.bias = self.base_module.bias
+        assert len(self.base_parameters) > 0, 'No base parameters found! Please add a base module of type torch.nn.Linear first.'
+        assert len(self.adapter_parameters) > 0, 'No adapter parameters found! Please either load an adapter or build one.'
+
+        out_features, in_features = self.base_parameters['weight'].size()
+
+        if 'bias' in self.base_parameters.keys():
+            merged_module = nn.Linear(in_features=in_features, out_features=out_features, bias=True)
+            merged_module.weight.data = self.base_parameters['weight'] + (
+                    (self.adapter_parameters['alpha'] / self.adapter_parameters['rank']) * torch.matmul(
+                self.adapter_parameters['delta_weight_B'], self.adapter_parameters['delta_weight_A']))
+            if 'delta_bias' in self.adapter_parameters.keys():
+                merged_module.bias.data = self.base_parameters['bias'] + (
+                        (self.adapter_parameters['alpha'] / self.adapter_parameters['rank']) * self.adapter_parameters['delta_bias'])
+            else:
+                merged_module.bias.data = self.base_parameters['bias']
+        else:
+            if 'delta_bias' in self.adapter_parameters.keys():
+                merged_module = nn.Linear(in_features=in_features, out_features=out_features, bias=True)
+                merged_module.weight.data = self.base_parameters['weight'] + (
+                        (self.adapter_parameters['alpha'] / self.adapter_parameters['rank']) * torch.matmul(
+                    self.adapter_parameters['delta_weight_B'], self.adapter_parameters['delta_weight_A']))
+                merged_module.bias.data = (
+                        (self.adapter_parameters['alpha'] / self.adapter_parameters['rank']) * self.adapter_parameters['delta_bias'])
+            else:
+                merged_module = nn.Linear(in_features=in_features, out_features=out_features, bias=False)
+                merged_module.weight.data = self.base_parameters['weight'] + (
+                        (self.adapter_parameters['alpha'] / self.adapter_parameters['rank']) * torch.matmul(
+                    self.adapter_parameters['delta_weight_B'], self.adapter_parameters['delta_weight_A']))
+
         return merged_module
 
     def forward(self, x: Tensor) -> Tensor:
+
         if not self.adapter_enabled:
-            return torch.matmul(x, self.base_module.weight.T) + self.base_module.bias
+            assert len(self.base_parameters) > 0, 'No base parameters found! Please add a base module of type torch.nn.Linear first.'
+            x = torch.matmul(x, self.base_parameters['weight'].T)
+            if 'bias' in self.base_parameters.keys():
+                x += self.base_parameters['bias']
         else:
-            if self.delta_bias is not None:
-                return torch.matmul(
-                    x,
-                    (self.base.weight + (self.alpha / self.rank) * torch.matmul(self.delta_weight_A, self.delta_weight_A)).T
-                ) + (self.base_module.bias + self.delta_bias)
-            else:
-                return torch.matmul(
-                    x,
-                    (self.base.weight + (self.alpha / self.rank) * torch.matmul(self.delta_weight_A, self.delta_weight_A)).T
-                ) + self.base_module.bias
+            assert len(self.base_parameters) > 0, 'No base parameters found! Please add a base module of type torch.nn.Linear first.'
+            assert len(self.adapter_parameters) > 0, 'No adapter parameters found! Please either load an adapter or build one.'
+            x = torch.matmul(
+                x,
+                (
+                        self.base_parameters['weight'] + (
+                        (self.adapter_parameters['alpha'] / self.adapter_parameters['rank']) * (
+                    torch.matmul(self.adapter_parameters['delta_weight_B'], self.adapter_parameters['delta_weight_A'])
+                )
+                )
+                ).T
+            )
+            if 'bias' in self.base_parameters.keys():
+                x += self.base_parameters['bias']
+            if 'delta_bias' in self.adapter_parameters.keys():
+                x += ((self.adapter_parameters['alpha'] / self.adapter_parameters['rank']) * self.adapter_parameters['delta_bias'])
+
+        return x
 
     def __repr__(self) -> str:
-        repr_string = f'LoRaLinear({self.base_module.__repr__()} + ((α={self.alpha}/r={self.rank}) × Adapter(in_features={self.in_features}, rank={self.rank}, out_features={self.out_features}, bias={True if self.delta_bias is not None else False})))'
+        repr_string = 'LoRALinear()'
+        if len(self.base_parameters) > 0:
+            out_features, in_features = self.base_parameters['weight'].size()
+            bias = 'bias' in self.base_parameters.keys()
+            base_repr_string = f'Linear(in_features={in_features}, out_features={out_features}, bias={bias})'
+
+            repr_string = f'LoRALinear({base_repr_string})'
+
+            if len(self.adapter_parameters) > 0:
+                alpha = self.adapter_parameters['alpha'].item()
+                rank = self.adapter_parameters['rank'].item()
+                delta_bias = 'delta_bias' in self.adapter_parameters.keys()
+                adapter_repr_string = f'Adapter(in_features={in_features}, rank={rank}, out_features={out_features}, delta_bias={delta_bias})'
+
+                repr_string = f'LoRALinear({base_repr_string} + ((α={alpha}/r={rank}) × {adapter_repr_string}))'
+
         return repr_string
